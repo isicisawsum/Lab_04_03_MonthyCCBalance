bacc3d31a54a848e6c2e16337740fa190d3eeab5
kregoric <75627074+isicisawsum@users.noreply.github.com>
2024-02-03 16:28:20 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,23 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        
         int ccBalance;
+        
         System.out.println("How much is on your credit card?"); //asks user to input for variable ccBalance
+        
         ccBalance = 5000; //simulated input
+        
         System.out.println("You entered $" + ccBalance); //outputting the balance user 'inputted'
+        
         double Interest = ccBalance * .17;//calculations for interest
+        
         ccBalance += Interest; //calculations for total balance
+        
         System.out.println("After one month your total is $" + ccBalance);
+        
         ccBalance += Interest; //more calculations to make month 2
+        
         System.out.println("After two months your total is $" + ccBalance);
-
     }
 }
